17f2c62825cea7cc864643d3d2265e7f6acf1ec0
maheshmuttintidev <maheshmuttintidev@gmail.com>
2025-06-20 15:51:06 +0530

    fix issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-
+import globals from "globals";
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
